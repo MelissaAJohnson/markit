c691cf194b68a33cb55c75c5aa47a227c26aaf0d
melissajstudent <missyj2007@gmail.com>
2016-08-18 18:25:43 -0500

    Updated incoming to check or create Topics and Bookmarks

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -8,28 +8,16 @@ class IncomingController < ApplicationController
     # Find the user by using params[:sender]
     @user = User.find_by(email: params[:sender])
 
-    # Find the topic by using params[:subject]
-    @topic = Topic.find_by(title: params[:subject])
-
-    # Assign the url to a variable after retreiving it from params["body-plain"]
-    @url = params["body-plain"]
-
     # Check if user is nil, if so, create and save a new user
     if @user.nil?
       @user = User.create(email: params[:sender], password: 'helloworld')
       @user.save
     end
 
-    # Check if the topic is nil, if so, create and save a new topic
-    if @topic.nil?
-      @topic = Topic.create(title: params[:subject], user_id: @user)
-      @topic.save
-    end
+    # Find the topic by using params[:subject]
+    @topic = Topic.find_or_create_by(title: params[:subject], user_id: @user.id)
+    @bookmark = @topic.bookmarks.find_or_create_by(url: params["body-plain"])
 
-    # Now that you're sure you have a valid user and topic, build and save a new bookmark
-    @bookmark = @topic.bookmarks.create(url: @url)
-
-    # Assuming all went well.
     head 200
  end
 end
