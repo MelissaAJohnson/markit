78064855c658ab7fbaf93adaf3cf4f88f01f0760
melissajstudent <missyj2007@gmail.com>
2016-08-18 18:10:57 -0500

    Removed devise auth on incoming and stopped test from sending emails

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -1,6 +1,8 @@
 class IncomingController < ApplicationController
 
   skip_before_action :verify_authenticity_token, only: [:create]
+  skip_before_action :authenticate_user!
+
 
   def create
     # Find the user by using params[:sender]
