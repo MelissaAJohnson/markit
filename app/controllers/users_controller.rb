67f40a61815826d06791d43782008aa999124c41
melissajstudent <missyj2007@gmail.com>
2016-08-22 19:03:41 -0500

    My Bookmarks complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,7 @@
 class UsersController < ApplicationController
   def show
     # populate @user_bookmarks with the user's bookmarks
-    @user_bookmarks = current_user.bookmarks
-    # @user_bookmarks = @user_bookmarks.all.group_by(&:topic_id)
+    @user_bookmarks = current_user.bookmarks.group_by { |s| s.topic }
 
     # populate @liked_bookmarks with liked bookmarks
     @liked_bookmarks = current_user.liked_bookmarks
